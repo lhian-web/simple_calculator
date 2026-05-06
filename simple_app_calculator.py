972b3b8e5f1e9ed5ca49a38e8625047f9cbba20e
unknown <jhullianchica@gmail.com>
2026-05-06 22:39:31 +0800

    Added user input handling and added if statement for ZeroDivisionError

--- simple_app_calculator.py
+++ simple_app_calculator.py
@@ -20,4 +20,15 @@ class Multiplication(Calculator):
 
 class Division(Calculator):
     def calculate(self):
+        if self.firstnumber != 0:
+            raise ZeroDivisionError("Cannot divide by zero")
         return self.firstnumber / self.secondnumber
+
+def get_num(self):
+    while True:
+        try:
+            first_number = float(input("Enter first number: "))
+            second_number = float(input("Enter second number: "))
+            return first_number, second_number
+        except ValueError:
+            print("Only numbers are accepted")
